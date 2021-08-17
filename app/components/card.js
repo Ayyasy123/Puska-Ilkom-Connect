0b0c06981b702f7816712a40270e4aa7718adb0a
User <syamilayyasy123@gmail.com>
2021-08-17 23:01:50 +0700

    setup react

---
+++ app/components/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const card = () => (
+  <View>
+    <Text>Card</Text>
+  </View>
+);
+
+export default card;
+
+const styles = StyleSheet.create({});
